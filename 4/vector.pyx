3a19de2296e5aa006cbedff50ef18d4e0231bce9
jpfigueredo <jp.figueredo8@gmail.com>
2024-12-12 17:49:48 -0300

    Feature: Finalizando os arquivos necessarios para o AT.

--- 4/vector.pyx
+++ 4/vector.pyx
@@ -1,15 +1,11 @@
-# vector.pyx
-from cython.parallel import parallel, prange
-cimport numpy as np  # cimport para numpy
-import numpy as np
+from cython.parallel import prange
+import cython
 
-# Função para multiplicar um vetor por um escalar
-def vector_by_scalar(np.ndarray[float, ndim=1] vector, double scalar):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def vector_by_scalar(double[:] vec, double scalar):
     cdef int i
-    cdef int n = len(vector)
-    
-    # Usando OpenMP para paralelismo com GIL liberado
-    with parallel():
-        with nogil:  # Liberar o GIL para permitir execução paralela
-            for i in prange(n):  # prange permite a execução paralela
-                vector[i] *= scalar
+    cdef int n = vec.shape[0]
+    with nogil:
+        for i in prange(n):
+            vec[i] *= scalar
